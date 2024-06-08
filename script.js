954e5216d0bc52243cf9f4a65f05d70e3a6ca662
yasuo055 <anonymousyasuo@gmail.com>
2024-06-08 10:50:00 +0800

    grid complete

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let x = 16;
-let y = 16;
+const main = document.querySelector("#container");
+
+
+
+function makeRows(rows, cols) {
+  main.style.setProperty("--grid-rows", rows);
+  main.style.setProperty("--grid-cols", cols);
+  for (let c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    //cell.innerText = (c + 1);
+    main.appendChild(cell).className = "grid-item";
+  }
+}
+
+makeRows(16, 16);
